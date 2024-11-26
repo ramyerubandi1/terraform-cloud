3167c263ba0796bbaf5cce97412c35ac27e83545
ramyerubandi1 <rmrbnd@gmail.com>
2024-11-26 07:46:23 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "AWS region"
-  default     = "us-west-2"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t2.micro"
-}

a58594db621a37d8bf77e84c26f2f7582fa37c0e
ramyerubandi1 <rmrbnd@gmail.com>
2024-11-26 11:21:44 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+# Define variables for the S3 bucket
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "bucket_acl" {
+  description = "The ACL to apply to the S3 bucket"
+  type        = string
+  default     = "private" # Default is private
+}
+
+variable "enable_versioning" {
+  description = "Enable versioning for the bucket"
+  type        = bool
+  default     = false
+}
+
+variable "environment" {
+  description = "The environment (e.g., dev, prod)"
+  type        = string
+  default     = "dev"
+}

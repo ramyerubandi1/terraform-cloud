4c1b38a9560cd21d7ab709037c58ea4e5c8976b7
ramyerubandi1 <rmrbnd@gmail.com>
2024-11-26 08:30:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,30 @@
-# Configure the provider (AWS)
 terraform {
+  cloud {
+    organization = "terraform-ram"
+
+    workspaces {
+      name = "terraform"
+    }
+  }
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.77.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-west-2"  # Change this to your desired region
+  region = "us-east-1" # Change to your preferred AWS region
 }
 
-# Use the S3 bucket module from the public Terraform Registry
-module "s3_bucket" {
-  source        = "terraform-aws-modules/s3-bucket/aws"  # Module source
-  bucket        = "my-unique-s3-bucket-name"             # Replace with your globally unique bucket name
-  acl           = "private"                              # Bucket access control list (ACL)
-  force_destroy = true                                   # Allow deleting non-empty buckets
-  versioning    = { enabled = true }                     # Enable versioning
+resource "aws_s3_bucket" "example" {
+  bucket = "example-bucket-name" # Change to your unique bucket name
+  acl    = "private"
+
+  tags = {
+    Name        = "example-bucket"
+    Environment = "Development"
+  }
 }

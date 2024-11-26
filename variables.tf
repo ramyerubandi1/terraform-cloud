b934775b68f2bc9d2fdb1d008670b6814a8a0281
ramyerubandi1 <rmrbnd@gmail.com>
2024-11-26 11:31:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
+# Define variables for the S3 bucket
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "default-example-bucket"
 }
 
 variable "bucket_acl" {

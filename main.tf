9b6f006acd88d933a5f0a378744957e2fbbc821a
ramyerubandi1 <rmrbnd@gmail.com>
2024-11-26 11:09:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,10 +21,15 @@ provider "aws" {
 
 resource "aws_s3_bucket" "example" {
   bucket = "example-bucket-ram-2024" # Replace with a globally unique name
-  acl    = "private"
 
   tags = {
     Name        = "example-bucket"
     Environment = "Development"
   }
 }
+
+resource "aws_s3_bucket_acl" "example_acl" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
+
